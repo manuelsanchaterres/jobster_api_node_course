63fca6936550218771d78207306a6401036f1bbb
mst736 <manuelsanchaterres@gmail.com>
2023-06-20 22:11:43 +0200

    first commit last course video 219

---
+++ app.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const path = require('path')
+
+// extra security packages
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors')
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.json());
+app.use(helmet());
+app.use(cors())
+app.use(xss());
+
+// routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+// serve index.html
+
+app.get('*', (req,res) => {
+
+
+  res.sendFile(path.resolve(__dirname,'./client/build', 'index.html'))
+
+})
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.LOCAL_SERVER_HTTP_PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI_DATABASE_JOBSTER_API );
+
+    console.log('Connect to JobsterApi database');
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
