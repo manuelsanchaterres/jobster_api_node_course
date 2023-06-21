1022fe5c85e53c02564d2175866f98d6eaa704bc
mst736 <manuelsanchaterres@gmail.com>
2023-06-21 03:41:23 +0200

    last functionality video 227

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/auth')
+const { register, login, updateUser } = require('../controllers/auth')
+const auth = require('../middleware/authentication')
 router.post('/register', register)
 router.post('/login', login)
-
+router.patch('/updateUser',auth, updateUser)
 module.exports = router
